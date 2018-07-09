af6459be917bc6bcd681d36c3e03cecfb4507c31
Patrick Dockstader <pdockstader@CIT-SCC112-1105.cs.dixie.edu>
2018-07-09 10:23:07 -0600

    Set up Game Logic File for testing logic cases

--- Logic Testing/client/main.js
+++ Logic Testing/client/main.js
@@ -1,5 +1,5 @@
 import data from './data.js'
-import {Piece, Pawn, Rook, Knight, Bishop, Queen, King} from './pieces.js'
+import {Piece, Pawn, Rook, Knight, Bishop, Queen, King, Blank} from './pieces.js'
 
 
 
@@ -44,15 +44,21 @@ const app = new Vue({
         
         getSelected: function(i, j){
             var image = this.getImage(i, j)
-            this.selected = image
-            console.log("clicked")
+            if (this.board[j-1][i-1].getId() != 0){
+                this.selected = this.board[j-1][i-1].getImg()
+                this.current = this.board[j-1][i-1]
+                return
+            }
+            else
+                this.movePiece(i, j)
+            console.log("called getSelected")
         },
         
         displayMoves: function(i, j){
-            console.log("called")
+            console.log("called displayMoves")
 
             for (var x = 0; x < this.moves.length; x++) { 
-                console.log(i,j , "=", this.moves[x][0],this.moves[x][1], x)
+                //console.log(i,j , "=", this.moves[x][0],this.moves[x][1], x)
                 if (i == this.moves[x][0] && j == this.moves[x][1])
                      return true
             }
@@ -62,13 +68,24 @@ const app = new Vue({
         getMoves: function(i, j){
             var in1 = i - 1
             var in2 = j - 1
-            console.log(this.board[in2][in1].getId())
+            console.log("called getMoves")
             this.moves = this.board[in2][in1].getMoves(i,j)
+            console.log(this.moves  )
             
-        }
+        },
         
-        
+        movePiece: function(i, j){
+            for(var move = 0; move < this.moves.length; move++){
+                if(this.moves[move][0] == i && this.moves[move][1] == j){
+                    this.board[j-1][i-1] = this.current
+                    // Need to delete old piece and change current piece x and ys 
+                    console.log(this.current.getX(), this.current.getY())
+                    this.board[this.current.getX()][this.current.getY()] = new Blank()
+                    this.current = new Blank()
+                }
+            }
 
+        }
     }
 
 })
