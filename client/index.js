847fbc9dbc88afdf0bb1f74b3157889f52d43bbb
cs 21 <cs21@CIT-SCC112-1104.cs.dixie.edu>
2018-07-10 13:08:17 -0600

    Fixed the Board so it is working, and started to add numbers and letters to the board

--- client/index.js
+++ client/index.js
@@ -1,1 +1,20 @@
-new Vue({ el: '#app' })
+const app = new Vue({ 
+	el: '#app' ,
+	methods: {
+		getColor: function (num, start) {
+            if (start % 2 == 0)    
+                if (num % 2 == 0)
+                    return "blue-grey lighten-4"
+
+                 else
+                     return "blue-grey darken-3" 
+                     
+            else
+                if (num % 2 == 0)
+                    return "blue-grey darken-3"
+
+                 else
+                     return "blue-grey lighten-4"
+        },
+	}
+})
