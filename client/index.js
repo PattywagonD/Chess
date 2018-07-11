c58e3151165a5f3439e739d647df5f316f49ba66
cs 21 <cs21@CIT-SCC112-1104.cs.dixie.edu>
2018-07-11 12:02:39 -0600

    Created the board and the numbers and started to impliment a starting screen and started to place the peices on the board

--- client/index.js
+++ client/index.js
@@ -1,5 +1,16 @@
 const app = new Vue({ 
 	el: '#app' ,
+	data: { board: [
+            [2,3,4,6,5,4,3,2],
+           [1,1,1,1,1,1,1,1],
+           [0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0],
+           [11,11,11,11,11,11,11,11],
+           [12,13,14,16,15,14,13,12]
+           ],
+       },
 	methods: {
 		getColor: function (num, start) {
             if (start % 2 == 0)    
@@ -16,5 +27,23 @@ const app = new Vue({
                  else
                      return "blue-grey lighten-4"
         },
-	}
-})
+        getImage: function (index1, index2){
+            var in1 = index1 - 1
+            var in2 = index2 - 1
+            return this.board[in2][in1].getImg()     
+        },
+        getSelected: function(i, j){
+            var image = this.getImage(i, j)
+            if (this.board[j-1][i-1].getId() != 0){
+                this.selected = this.board[j-1][i-1].getImg()
+                this.current = this.board[j-1][i-1]
+                return
+            }
+            else
+                this.movePiece(i, j)
+            console.log("called getSelected")
+        },
+        }
+	})
+
+
